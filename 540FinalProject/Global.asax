3a5bae88e63d268525d42d2f6d5ea953cc554319
Marlin Davis <madavis@EMAIL.USCUPSTATE.EDU>
2018-10-30 15:30:17 -0400

    Add project files.

---
+++ 540FinalProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_540FinalProject.MvcApplication" Language="C#" %>
---
+++ 540FinalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_540FinalProject.Startup))]
+namespace _540FinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
